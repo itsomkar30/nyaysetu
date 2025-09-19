d928298ab966ae37a16c49afe7823d3df25fe5b0
Omkar P <itsomkar30@gmail.com>
2025-09-19 20:14:05 +0530

    added multi language gradle for mlkit ocr

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,7 +42,13 @@ android {
         }
     }
 }
-
+dependencies {
+    // ML Kit Text Recognition dependencies (Kotlin DSL syntax)
+    implementation("com.google.mlkit:text-recognition-chinese:16.0.0")
+    implementation("com.google.mlkit:text-recognition-devanagari:16.0.0")
+    implementation("com.google.mlkit:text-recognition-japanese:16.0.0")
+    implementation("com.google.mlkit:text-recognition-korean:16.0.0")
+}
 flutter {
     source = "../.."
 }
